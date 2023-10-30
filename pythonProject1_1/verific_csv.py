144317e1a87cb96c75a0324b61b3ba89a07b9d82
Acederys <Acederys@mail.ru>
2023-10-30 11:35:59 +0500

    corrected mistake of double in result

--- pythonProject1_1/verific_csv.py
+++ pythonProject1_1/verific_csv.py
@@ -1,43 +1,33 @@
 import fasttext
 import csv
 model = fasttext.load_model('pyfasttext/models/lid.176.bin')
+file = ['result_ataisal.csv', 'result_bash.csv', 'result_bashgazet.csv','result_kulturarb.csv', 'result_ye102.csv']
 
-clear_row = list()
-
-file_1 = 'result_ataisal.csv'
-file_2 = 'result_bash.csv'
-file_3 = 'result_bashgazet.csv'
-file_4 = 'result_kulturarb.csv'
-file_5 = 'result_ye102.csv'
-def row_reader(file_f):
-    with open(file_f, 'r', encoding='utf-8') as data:
+clear_full_url = list()
+for url in file:
+    with open(url, 'r', encoding='utf-8') as data:
         reader = csv.reader(data)
         for row in reader:
-            row_str = row[2].strip().replace('\n',' ')
-            lid_result = model.predict(row_str, 3)
-            if lid_result[0][0] == '__label__ba':
-                dict_content = {
-                    'url': row[0],
-                    'title': row[1],
-                    'lead': row[2],
-                    'content': row[3],
-                    'data': row[4],
-                    'category': row[5],
-                    'tags': row[6]
-                }
+            dict_content = {
+                'url': row[0],
+                'title': row[1],
+                'lead': row[2],
+                'content': row[3],
+                'data': row[4],
+                'category': row[5],
+                'tags': row[6]
+            }
+            clear_full_url.append(dict_content)
 
-                clear_row.append(dict_content)
-    return clear_row
+list_item = list()
+for elem in clear_full_url:
+    row_str = elem['content'].strip().replace('\n',' ')
+    lid_result = model.predict(row_str, 3)
+    if lid_result[0][0] == '__label__ba':
+        list_item.append(elem)
 
-result_ataisal = row_reader(file_1)
-result_bash = row_reader(file_2)
-result_bashgazet = row_reader(file_3)
-result_kulturarb = row_reader(file_4)
-result_ye102 = row_reader(file_5)
-
-full_result = result_ataisal + result_bash + result_bashgazet + result_kulturarb + result_ye102
 fieldnames = ['url', 'title', 'lead', 'content', 'data', 'category', 'tags']
-with open(r'clear_result.csv', 'w', encoding='UTF8', newline='') as f:
+with open(r'clear_result_test.csv', 'w', encoding='UTF8', newline='') as f:
     writer = csv.DictWriter(f, fieldnames=fieldnames)
     writer.writeheader()
-    writer.writerows(full_result)
+    writer.writerows(list_item)
